cd79433f89b9b8e344967d48b54f96c14859cd5c
Yu Sun <809186577@qq.com>
2019-08-15 12:06:43 +0800

    Create Setting.cs

---
+++ DisplaySystem/Assets/Scripts/Setting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Material skybox;
+    private float rotationSpeed = 0.5f;
+    void Start() {
+        skybox = RenderSettings.skybox;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skyBoxRotate();
+    }
+
+    //天空盒自旋转
+    private void skyBoxRotate() {
+        float value = (Time.time * rotationSpeed)%360;
+        skybox.SetFloat("_Rotation" , value);
+    }
+}
