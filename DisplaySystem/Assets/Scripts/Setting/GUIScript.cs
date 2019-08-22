2b40bfb671f4d3dfa3fbc0eb33de43ab5ca422db
Yu Sun <809186577@qq.com>
2019-08-22 15:56:28 +0800

    Create GUIScript.cs

---
+++ DisplaySystem/Assets/Scripts/Setting/GUIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIScript : UIClass {
+    public Vector2 initialPos;
+    public Vector2 targetPos;
+
+
+    private void Awake() {
+        SetInitialPos(initialPos);
+    }
+
+    void Update()
+    {
+        RCMove(targetPos);
+    }
+        
+
+
+
+}
