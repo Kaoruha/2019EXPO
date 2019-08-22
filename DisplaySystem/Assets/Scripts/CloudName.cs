172944d18d6bf66fcf72ade63bbbf1a316ee1f0f
Yu Sun <809186577@qq.com>
2019-08-22 21:41:18 +0800

    Create CloudName.cs

---
+++ DisplaySystem/Assets/Scripts/CloudName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudName : MonoBehaviour {
+    private Transform cam;
+    private Vector3 lookAtPos;
+    void Start() {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        LookAt();
+        
+    }
+
+
+    private void LookAt() {
+        lookAtPos = transform.position - (cam.position - transform.position);
+        transform.LookAt(lookAtPos);
+    }
+}
