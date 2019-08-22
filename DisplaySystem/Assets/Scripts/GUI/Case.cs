b9348734d715038e924778509c41abc096b00b18
Yu Sun <809186577@qq.com>
2019-08-22 18:02:03 +0800

    Create Case.cs

---
+++ DisplaySystem/Assets/Scripts/GUI/Case.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class Case : UIClass
+{
+    public Vector2 initialPos;
+    private Vector2 targetPos;
+    private float zoomRate;
+
+    private void Awake() {
+        zoomRate = 1f;
+        targetPos = new Vector2(960,640);
+    }
+}
