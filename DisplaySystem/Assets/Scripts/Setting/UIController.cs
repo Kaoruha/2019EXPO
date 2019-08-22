23e8c42c54abe585bbf16b55f49be9377db150c4
Yu Sun <809186577@qq.com>
2019-08-22 15:56:23 +0800

    Update UIController.cs

--- DisplaySystem/Assets/Scripts/Setting/UIController.cs
+++ DisplaySystem/Assets/Scripts/Setting/UIController.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class UIController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class UIController : MonoBehaviour {
+    public List<GUIScript> GUIComponents;
+    public Color frameColor;
+    private int count;
+    private float interval;
+    
+
+    private void Awake() {
+        interval = 0.05f;
+        count = transform.childCount;
+        for (int i = 0; i < count; i++) {
+            GUIComponents.Add(transform.GetChild(i).GetComponent<GUIScript>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        ShouldIn();
     }
+
+    public void ShouldIn() {
+        StartCoroutine(MoveIn());
+    }
+    
+    public void ShouldOut() {
+        StartCoroutine(MoveOut());
+    }
+
+
+    private IEnumerator MoveIn() {
+        for (int i = 0; i < count; i++) {
+            GUIComponents[i].SetState(UIClass.RCUIState.shouldIN);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    private IEnumerator MoveOut() {
+        for (int i = count-1; i >= 0; i--) {
+            GUIComponents[i].SetState(UIClass.RCUIState.shouldOut);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+    
+    
+
+
 }
