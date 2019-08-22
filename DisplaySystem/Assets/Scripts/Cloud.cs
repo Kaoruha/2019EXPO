dbfa8fc0cf6216094e8300edaaf423fe5cadaed6
Yu Sun <809186577@qq.com>
2019-08-22 21:41:15 +0800

    Create Cloud.cs

---
+++ DisplaySystem/Assets/Scripts/Cloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cloud : MonoBehaviour {
+    private RaycastHit hitInfo;
+    public Case shownCase;
+    void Update(){
+        
+        if (Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo, 9999f)){
+                if ((hitInfo.transform == transform) && !EventSystem.current.IsPointerOverGameObject()){
+                    Debug.Log(hitInfo.transform);
+                    shownCase.MoveIn();
+                }
+            }
+        }
+    }
+}
