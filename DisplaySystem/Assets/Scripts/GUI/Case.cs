b5d75f4b032eb867b36a8a55ccab3847be1e6312
Yu Sun <809186577@qq.com>
2019-08-23 19:09:18 +0800

    Update Case.cs

--- DisplaySystem/Assets/Scripts/GUI/Case.cs
+++ DisplaySystem/Assets/Scripts/GUI/Case.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
@@ -9,13 +9,17 @@ public class Case : UIClass {
     private Vector2 initialPos;
     private Vector2 targetPos;
     public UIController uiController;
-    private float _zoomSpeed = 3f;
+    private float zoomRateWithDis = 1.2f;
 
     private float interval = 0.01f;
     private float targetZoom = 0f;
     private Vector3 screenPos;
+    private Vector2 screenCenter = new Vector2();
+    private float distance;
+    private float targetSpeed;
 
     private void Awake() {
+        screenCenter = new Vector2(UnityEngine.Screen.width/2,UnityEngine.Screen.height/2);
         transform.localScale = new Vector3(0,0,0);
         
         
@@ -25,7 +29,9 @@ public class Case : UIClass {
     public void MoveIn() {
         screenPos = Camera.main.WorldToScreenPoint(btnFrom.position);
         initialPos = new Vector2(screenPos.x,screenPos.y);
-        SetInitialPos(initialPos);
+        distance = Vector2.Distance(screenCenter, screenPos);
+        ChangeSpeed();
+        SetInitialPos(initialPos,true);
         uiController.ShouldOut();
         SetState(RCUIState.shouldIN);
         StartCoroutine(ZoomIn());
@@ -34,7 +40,8 @@ public class Case : UIClass {
     public void MoveOut() {
         screenPos = Camera.main.WorldToScreenPoint(btnFrom.position);
         initialPos = new Vector2(screenPos.x,screenPos.y);
-        SetInitialPos(initialPos);
+        ChangeSpeed();
+        SetInitialPos(initialPos, false);
         uiController.ShouldIn();
         SetState(RCUIState.shouldOut);
         StartCoroutine(ZoomOut());
@@ -48,7 +55,8 @@ public class Case : UIClass {
 
     private IEnumerator ZoomIn() {
         while (targetZoom <= 1f) {
-            targetZoom += Time.deltaTime * _zoomSpeed;
+            targetZoom = (Vector2.Distance(transform.position, initialPos)) / (Vector2.Distance(initialPos, targetPos));
+                
             transform.localScale = new Vector3(targetZoom,targetZoom,1);
             yield return new WaitForSeconds(interval);
         }
@@ -57,10 +65,15 @@ public class Case : UIClass {
 
     private IEnumerator ZoomOut() {
         while (targetZoom >= 0f) {
-            targetZoom -= Time.deltaTime * _zoomSpeed;
+            targetZoom = (Vector2.Distance(transform.position, initialPos)) / (Vector2.Distance(initialPos, targetPos));
             transform.localScale = new Vector3(targetZoom,targetZoom,1);
             yield return new WaitForSeconds(interval);
         }
         transform.localScale = new Vector3(0,0,0);
+    }
+
+    private void ChangeSpeed() {
+        targetSpeed = 1400f * distance / 260f;
+        SetMoveSpeed(targetSpeed);
     }
 }
